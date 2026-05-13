075af4bb3ae2099c66f63b81ff06996d2911162b
EliteShadow83 <52933342+EliteShadow83@users.noreply.github.com>
2026-05-13 18:42:04 -0500

    Update project for Gradle 10 compatibility

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    // Android Gradle Plugin and Kotlin versions aligned for newer Gradle runtimes.
+    id("com.android.application") version "8.8.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+}
